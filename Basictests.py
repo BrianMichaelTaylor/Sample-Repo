7e90df8eb1a8bde27bd399fb646776dd5467571e
Brian Taylor <brianmtaylor22@gmail.com>
2019-09-08 01:21:01 -0400

    Added basic test

---
+++ Basictests.py
@@ -0,0 +1,16 @@
+message = "Hello World"
+
+new_message = message.replace("World", "Universe")
+
+
+print(message[0:4])
+print(message.count("Hello"))
+print(new_message)
+
+intro = "Game"
+outro = "Time"
+
+broadcast = f"{intro}, {outro.upper()}. Lets GO!"
+print(broadcast)
+
+# print(dir(outro))
